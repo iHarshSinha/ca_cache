aa5f32cfe2d8f9465a4d0c3741194af2934f3850
harsh sinha <harsh.sinha@iiitb.ac.in>
2024-08-28 14:55:17 +0530

    first draft

---
+++ driver.py
@@ -0,0 +1,7 @@
+class cache():
+    '''this class represent cache level 1'''
+    pass
+class set():
+    '''this class represent various sets or lines in the cache'''
+class way():
+    '''this class represent ways which will actually hold the data in the cache'''

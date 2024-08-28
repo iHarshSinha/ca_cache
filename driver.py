80ca8048df125afab44f7ca972f47fbb977ef3d2
NIPUN <nipun.verma@iiitb.ac.in>
2024-08-28 16:32:46 +0530

    Completed the tag and Set class

--- driver.py
+++ driver.py
@@ -1,26 +1,38 @@
-class Way():
+class Way:
     '''this class represent ways which will actually hold the data in the cache'''
 
-    def __init__(self, tag, data):
+    def __init__(self):
+        self.dirty_bit = 0
+        self.tag = ""
+        self.data = ""
+
+    def add_data(self, tag, data):
+        self.dirty_bit = 1
         self.tag = tag
         self.data = data
-        self.dirty_bit = 0
+
+    def evict_data(self, new_tag, new_data):
+        if self.dirty_bit:
+            self.dirty_bit = 0
+            self.tag = ""
+            self.data = ""
+            self.add_data(new_tag, new_data)
 
 
-class Set():
+class Set:
     '''this class represent various sets or lines in the cache'''
 
     def __init__(self, ways):
         self.way = ways
-        self.list = []      #list for way object
+        self.way_list = []  #list for way object
 
-    def addways(self,tag,data):
+    def add_ways(self):
         i = 0
-        while i < self.way:     #initializing a list of ways for a particular set
-            way_obj = Way(tag,data)
-            self.list.append(way_obj)
+        while i < self.way:  #initializing a list of ways for a particular set
+            way_obj = Way()
+            self.way_list.append(way_obj)
 
 
-class Cache():
+class Cache:
     '''this class represent cache level 1'''
     pass

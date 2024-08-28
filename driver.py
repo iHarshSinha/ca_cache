14d31cddcadb9115dcf638916ddc46d4af6857e3
NIPUN <nipun.verma@iiitb.ac.in>
2024-08-28 16:03:48 +0530

    Set class made

--- driver.py
+++ driver.py
@@ -1,7 +1,26 @@
+class way():
+    '''this class represent ways which will actually hold the data in the cache'''
+
+    def __init__(self, tag, data):
+        self.tag = tag
+        self.data = data
+        self.dirty_bit = 0
+
+
+class set():
+    '''this class represent various sets or lines in the cache'''
+
+    def __init__(self, ways):
+        self.way = ways
+        self.list = []
+
+    def addways(self):
+        i = 0
+        while i < self.way:     #initializing a list of ways for a particular set
+            way_obj = way(tag,data)
+            self.list.append(way_obj)
+
+
 class cache():
     '''this class represent cache level 1'''
     pass
-class set():
-    '''this class represent various sets or lines in the cache'''
-class way():
-    '''this class represent ways which will actually hold the data in the cache'''
